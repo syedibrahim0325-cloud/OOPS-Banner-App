0cd3fec18907fd81fa3c0ab8a8404edfde2558a2
syedibrahim0325-cloud <syedibrahim0325@gmail.com>
2026-02-15 14:59:49 +0530

    UC2: Implement banner formatting logic

---
+++ OOPSBannerApp2.java
@@ -0,0 +1,15 @@
+ public class OOPSBannerApp2
+{
+public static void main(String args[])
+{
+System.out.println("      ******      "   +   "       ******      "   +   "  ********      "   +   "     ******      " );
+System.out.println("    **      **    "   +   "     **      **    "   +   "  **      **    "   +   "   **            " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  **        **  "   +   "  **             " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  **      **    "   +   "    **           " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  ** ******     "   +   "      ****       " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  **            "   +   "           **    " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  **            "   +   "            **   " );
+System.out.println("    **      **    "   +   "     **      **    "   +   "  **            "   +   "          **     " );
+System.out.println("      ******      "   +   "       ******      "   +   "  **            "   +   "    ******       " );
+}
+}
