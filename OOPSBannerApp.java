25ecd3f0c06c84c4e09bb6a055f69312f73054a0
syedibrahim0325-cloud <syedibrahim0325@gmail.com>
2026-02-15 14:16:00 +0530

    UC1: Initial implementation of OOPS print

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+/**
+ * @author Ibrahim
+ * @version 1.0
+ * Implementation of Use Case 1: Printing "OOPS" to the console.
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // This prints the literal text OOPS to the console
+        System.out.println("OOPS");
+    }
+}
