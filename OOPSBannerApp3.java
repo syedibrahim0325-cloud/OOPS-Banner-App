5c30441c1a5fa23865fc3e0349f774a31a755161
syedibrahim0325-cloud <syedibrahim0325@gmail.com>
2026-02-16 13:49:08 +0530

    Added UC3 - OOPSBannerApp3 implementation

---
+++ OOPSBannerApp3.java
@@ -0,0 +1,15 @@
+ public class OOPSBannerApp3 
+{
+public static void main(String args[])
+{
+System.out.println(String.join(" ","     ******      "   ,   "       ******      "   ,   "  ********      "   ,   "     ******      " ));
+System.out.println(String.join(" ","   **      **    "   ,   "     **      **    "   ,   "  **      **    "   ,   "   **            " ));
+System.out.println(String.join(" "," **          **  "   ,   "   **          **  "   ,   "  **        **  "   ,   "  **             " ));
+System.out.println(String.join(" "," **          **  "   ,   "   **          **  "   ,   "  **      **    "   ,   "    **           " ));
+System.out.println(String.join(" "," **          **  "   ,   "   **          **  "   ,   "  ** ******     "   ,   "      ****       " ));
+System.out.println(String.join(" "," **          **  "   ,   "   **          **  "   ,   "  **            "   ,   "           **    " ));
+System.out.println(String.join(" "," **          **  "   ,   "   **          **  "   ,   "  **            "   ,   "            **   " ));
+System.out.println(String.join(" ","   **      **    "   ,   "     **      **    "   ,   "  **            "   ,   "          **     " ));
+System.out.println(String.join(" ","     ******      "   ,   "       ******      "   ,   "  **            "   ,   "    ******       " ));
+}
+}
