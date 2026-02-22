e1cc0905c84651bdd1bfcfb89956524543c1ca8c
syedibrahim0325-cloud <syedibrahim0325@gmail.com>
2026-02-23 00:45:50 +0530

    OOPSBannerApp6 file commited

---
+++ OOPSBannerApp5.java
@@ -0,0 +1,18 @@
+public class OOPSBannerApp5 
+{
+public static void main(String args[])
+{
+System.out.println(String.join(" ","     ******      "   ,   "       ******      "   ,   "  ********      "   ,   "     ******      " ));
+System.out.println(String.join(" ","   **      **    "   ,   "     **      **    "   ,   "  **      **    "   ,   "   **            " ));
+System.out.println(String.join(" "," **          **  "   ,   "   **          **  "   ,   "  **        **  "   ,   "  **             " ));
+System.out.println(String.join(" "," **          **  "   ,   "   **          **  "   ,   "  **      **    "   ,   "    **           " ));
+System.out.println(String.join(" "," **          **  "   ,   "   **          **  "   ,   "  ** ******     "   ,   "      ****       " ));
+System.out.println(String.join(" "," **          **  "   ,   "   **          **  "   ,   "  **            "   ,   "           **    " ));
+System.out.println(String.join(" "," **          **  "   ,   "   **          **  "   ,   "  **            "   ,   "            **   " ));
+System.out.println(String.join(" ","   **      **    "   ,   "     **      **    "   ,   "  **            "   ,   "          **     " ));
+System.out.println(String.join(" ","     ******      "   ,   "       ******      "   ,   "  **            "   ,   "    ******       " ));
+}
+}
+
+    
+
